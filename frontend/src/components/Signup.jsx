e2dc53547ac946095c2280794cbb487f62441099
jatin foujdar <jatin321001@gmail.com>
2022-12-15 22:52:00 +0530

    second commit

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Signup = () => {
-  return (
+  return ( 
     <div class="bg-gray-700">
     <div class="m-auto xl:container px-12 sm:px-0 mx-auto">
     <div class="mx-auto h-full sm:w-max">
@@ -19,15 +19,12 @@ const Signup = () => {
             class="w-full h-11 rounded-full border border-gray-300/75 bg-white px-6 transition active:bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-800 dark:hover:border-gray-700"
           >
             <div class="w-max mx-auto flex items-center justify-center space-x-4">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
+            <svg xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
                 class="w-5"
                 viewBox="0 0 16 16"
               >
-                <path
-                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-                />
+               <path d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
               </svg> 
               <span class="block w-max text-sm font-semibold tracking-wide text-cyan-700 dark:text-white"
                 >With Google</span
